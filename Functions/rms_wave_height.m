6c2a63f3b10eed7d649c0344e9a1bd2234a554f5
CTaaban <ctaaban@outlook.com>
2018-11-16 22:57:51 +0100

    Delete rms_wave_height.m

--- Functions/rms_wave_height.m
+++
@@ -1,6 +0,0 @@
-function Hrms = rms_wave_height(Hind)
-% Hrms = rms_wave_height(Hind)
-% input    Hind array containing the individual wave heights in m 
-% output   Hrms root mean square wave height in m.
-
-Hrms = sqrt(mean(Hind.^2)); % calculation of Hrms (NB: sqrt is a matlab built-in function that calculate the root square)
